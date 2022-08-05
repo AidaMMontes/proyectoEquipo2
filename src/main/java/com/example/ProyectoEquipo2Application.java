ae032722bc3c448693b6b7bb75598fe273116b4c
Aida-mm <aidamoyamontes@gmail.com>
2022-08-05 14:14:51 +0200

    Inicio Proyecto

---
+++ src/main/java/com/example/ProyectoEquipo2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEquipo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEquipo2Application.class, args);
+	}
+
+}
