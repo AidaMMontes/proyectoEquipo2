b0e8134887ad6d73bcfa0d47318b80b5fc1dc841
rverac <irhis13@gmail.com>
2022-08-10 18:36:07 +0200

    empleado atributos

--- src/main/java/com/example/entities/Empleado.java
+++ src/main/java/com/example/entities/Empleado.java
@@ -1,5 +1,11 @@
 package com.example.entities;
 
 public class Empleado {
+
+    private long idEmpleado;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    
     
 }
