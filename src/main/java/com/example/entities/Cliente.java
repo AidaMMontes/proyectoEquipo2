383d22b330a35196734b1a48ca9363767d496b6c
Asun <asunosa91@gmail.com>
2022-08-10 18:29:03 +0200

    EntidadCliente

--- src/main/java/com/example/entities/Cliente.java
+++ src/main/java/com/example/entities/Cliente.java
@@ -1,5 +1,57 @@
 package com.example.entities;
+/*Cliente: (@ManyToOne(Un cliente puede viajar a 1 ó muchos destinos)
+-          ID (int)
+-          Nombre (String)
+-          Apellidos (String)
+-          DNI escaneado
+-          Fecha de Salida (Local Date)
+-          Fecha de Regreso (Local Date) 
+-          Teléfono (String)
+-          Email (String)
+-          Destino */
 
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+// @Getter
+// @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Cliente {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombre;
+    private String apellidos;
+    private String imagenDni;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate fechaSalida;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate fechaRegreso;
+    private String telefono;
+    private String email;
+
+    @ManyToOne
+    @NotNull
+    private Viaje viaje;
+
 }
